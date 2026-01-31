fbd3191a4726c003367dab7b4d3dff656d2ee808
deepu2005g-del <deepu2005g@gmail.com>
2026-01-31 12:58:19 +0530

    CNN flask demo

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/<name>")
+def name(name):
+    return render_template("name.html",name=name)
+
+@app.route("/abc")
+def abc():
+    return render_template("abc.html")
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=5000,use_reloader=False)
